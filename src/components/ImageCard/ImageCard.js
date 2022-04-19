672c5282571094885a0efa0ff91c27170fbe9f87
zahra <alzahraam209@gmail.com>
2022-04-19 23:53:25 +0200

    add icon and brightness over each img

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,10 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlassPlus } from '@fortawesome/free-solid-svg-icons';
 import './ImageCard.scss'
 const ImageCard = ({imgSrc}) =>{
     
     return(
         <>
-        <div className="mb-2 imageDiv">
-            <img width='300' height={200} src={imgSrc} alt="imag" />
+        <div className="mb-2 position-relative">
+            {/* <div className='overlayImg position-absolute'></div> */}
+            <img className='imageDiv' width='300' height={200} src={imgSrc} alt="imag" />
+        
+            <FontAwesomeIcon className='magnifyIcon' icon={faMagnifyingGlassPlus} />
+            
         </div>
         </>
     )
